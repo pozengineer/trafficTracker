8b36bd1fb792e92eddee23da8ac16c97f5450add
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-03-08 00:52:18 +1100

    heroku deployment

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,27 @@
 // Export routes for server.js to use.
 var path = require("path");
+var db = require("../models")
 
 // Routes
 // =============================================================
 module.exports = function (app) {
 
-  // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../views/startPage.html"));
-  // });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/startPage.html"));
+  });
 
   app.get("/favicon.ico", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/images/favicon.ico/favicon.ico"));
   });
 
-  app.get("/", function(req, res) {
-    res.render("index");
+  app.get("/app", function(req, res) {
+    db.User.findAll({}).then(function (data) {
+      var hbsObject = {
+          users: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    })
   });
   
   // app.get("/app", function(req, res) {
